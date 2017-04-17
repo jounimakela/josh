e90bdcbb1d8ac6d0cd32ae5bd76f00efc31b455c
pabn1 <root@jounimakela.com>
2017-04-17 22:07:44 +0300

    Enable raw mode
    Signed-off-by: jouni <jouni.img@gmail.com>

--- main.c
+++ main.c
@@ -1,4 +1,41 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void tty_restore()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void tty_raw_mode()
+{
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(tty_restore);
+
+	struct termios raw = orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	/* raw.c_oflag &= ~(OPOST); */
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main()
 {
+	tty_raw_mode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }

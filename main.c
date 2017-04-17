0e8c8ac7ae33b5862bd18a0354a3e9bd9c0ef7e3
pabn1 <root@jounimakela.com>
2017-04-17 23:39:35 +0300

    Refactored input reading
    Signed-off-by: jouni <jouni.img@gmail.com>

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define CTRL(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s)
@@ -27,25 +29,55 @@ void tty_raw_mode()
 
 	struct termios raw = orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	/* raw.c_oflag &= ~(OPOST); */
+	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
 
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
 		die("tcsetattr");
+}
+
+char read_key()
+{
+	char c;
+	while (read(STDIN_FILENO, &c, 1) != 1);
+	return c;
+}
+
+void process_key()
+{
+	char c = read_key();
+
+	switch(c) {
+		case CTRL('q'):
+			exit(0);
+			break;
+
+		case CTRL('h'):
+			write(STDOUT_FILENO, "\x1b[1D", 4);
+			break;
+
+		case CTRL('l'):
+			write(STDOUT_FILENO, "\x1b[1C", 4);
+			break;
+
+		default:
+			if (isprint(c)) {
+				printf("%c", c);
+				fflush(stdout);
+			}
+			break;
+	}
 }
 
 int main()
 {
 	tty_raw_mode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		if (iscntrl(c)) {
-			printf("%d\n", c);
-		} else {
-			printf("%d ('%c')\n", c, c);
-		}
+	while (1) {
+		process_key();
 	}
+
 	return 0;
 }
